763d9ef7cc1a40866a6a34e8448a657cdc795096
Brandon Hoffman <Precision.bh@gmail.com>
2026-05-31 20:16:07 -0400

    favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import "@/styles/globals.css"
+import type { AppProps } from "next/app"
+import Head from "next/head"
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App({
+  Component,
+  pageProps,
+}: AppProps) {
+  return (
+    <>
+      <Head>
+        <link
+          rel="icon"
+          href="/precision-logo.png"
+        />
+      </Head>
+
+      <Component
+        {...pageProps}
+      />
+    </>
+  )
 }
